e3e98a5a9919a5035bcbc74e7035b1c25755897e
asdiop159752 <nxt159753@gmail.com>
2023-06-30 16:55:14 +0700

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import { Figtree } from 'next/font/google'
 
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Sidebar>
         {children}
+        </Sidebar>
       </body>
     </html>
   )
