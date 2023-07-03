ba04bb784cffe99311f42c32e2231f529bed07cd
asdiop159752 <nxt159753@gmail.com>
2023-07-03 11:48:14 +0700

    Stripe integration

---
+++ app/(site)/error.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Box from "@/components/Box";
+
+const Error = () => {
+    return ( 
+        <Box className="h-full flex items-center justify-center">
+            <div className="text-neutral-400">
+                    Song thing went wrong
+            </div>
+        </Box>
+     );
+}
+ 
+export default Error;
+<div>
+
+</div>
